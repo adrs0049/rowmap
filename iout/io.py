4cb97bef8ca94fa72de2960036a37d8e8d1b0a99
Andreas Buttenschoen <andreas@buttenschoen.ca>
2018-04-11 19:55:19 -0700

    Write lots of writeHDF5 functions

--- iout/io.py
+++ iout/io.py
@@ -10,3 +10,7 @@ def writeDataFrame(filename, dataframe):
     dataframe.to_hdf(filename, dataframe.name, mode='a', table=True)
 
 
+def print_attrs(name, obj):
+    print(name)
+    for key, val in obj.attrs.iteritems():
+        print("    %s: %s" % (key, val))

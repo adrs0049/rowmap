6ab601866c3fa560fdb3fe224d9985bf2abb37ae
Andreas Buttenschoen <andreas@buttenschoen.ca>
2018-06-06 12:47:12 -0700

    io: add some useful functions to get node from xml

--- iout/io.py
+++ iout/io.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from lxml import etree
 #import h5py
 #import numpy as np
 
@@ -14,3 +15,17 @@ def print_attrs(name, obj):
     print(name)
     for key, val in obj.attrs.iteritems():
         print("    %s: %s" % (key, val))
+
+
+def getXMLNode(tree, name):
+    root = tree.getroot()
+    for child in root:
+        if child.tag == name:
+            return child
+
+
+def parseXML(xmlFile, name='Simulator'):
+    parser = etree.XMLParser(remove_comments=True)
+    tree   = etree.parse(xmlFile, parser)
+    return getXMLNode(tree, name)
+
